1c331b9a961291b9cb370792f8ab21484981de04
KoroneSS <82208968+KoroneSS@users.noreply.github.com>
2025-02-26 19:51:58 +0700

    Update main.java

--- main.java
+++ main.java
@@ -2,6 +2,7 @@ public class main {
     public static void main(String[] args){
         vehicle v = new vehicle(1000, "Toyota", "Good", 2002, "Toyota", 10000);
         System.out.println(v.toString());
+        System.out.println("hi");
     }
     
 }
